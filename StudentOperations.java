e0ddb54eb1f9bf229c42d4f271b207ffc0bb5217
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-27 12:13:42 +0530

    add method to display all students in StudentOperations class

--- StudentOperations.java
+++ StudentOperations.java
@@ -17,13 +17,23 @@ public class StudentOperations {
         sc.nextLine();
         System.out.println("Enter Name: ");
         String name = sc.nextLine();
-        System.out.println("Enter DOB: ");
+        System.out.println("Enter DOB (yyyy-mm-dd): ");
         String dob = sc.nextLine();
         System.out.println("Enter Marks: ");
         double marks = sc.nextDouble();
 
         students.add(new Student(prn, name, dob, marks));
         System.out.println("Student added successfully!");
-        
+    }
 
+    // method to display all students
+    public void displayStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students to display!");
+            return;
+        }
+        for (Student student : students) {
+            student.displayStudentDetails();
+        }
+    }
 }
