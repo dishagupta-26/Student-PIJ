b43d5cb662062387c5d8ca1c3de30782ab5b56d5
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-27 11:23:17 +0530

    declare instance members in Student class

--- Student.java
+++ Student.java
@@ -1,5 +1,9 @@
 import java.util.Date;
 
 public class Student {
-
+    // declaring the instaance members
+    private int prn;
+    private String name;
+    private Date dob;
+    private double marks;
 }
