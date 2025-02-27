5439c4ce3b37f37dd8241151aa3911f7fcd385e2
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-27 12:08:49 +0530

    add method to add student in StudentOperations class

--- StudentOperations.java
+++ StudentOperations.java
@@ -10,4 +10,16 @@ public class StudentOperations {
         students = new ArrayList<>();
     }
 
+    // method to add student
+    public void addStudent(Scanner sc) {
+        System.out.println("Enter PRN: ");
+        long prn = sc.nextLong();
+        sc.nextLine();
+        System.out.println("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter DOB: ");
+        String dob = sc.nextLine();
+        System.out.println("Enter Marks: ");
+        double marks = sc.nextDouble();
+
 }
