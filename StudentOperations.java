808aa2aafaf3ca84de51188dbd10a70165c1ae28
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-27 12:05:38 +0530

    add ArrayList to manage students in StudentOperations class

--- StudentOperations.java
+++ StudentOperations.java
@@ -3,5 +3,11 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class StudentOperations {
+    private ArrayList<Student> students;
+
+    // constructor
+    public StudentOperations() {
+        students = new ArrayList<>();
+    }
 
 }
