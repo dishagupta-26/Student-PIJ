29c7b230157c81dcf6d59f376c95d7dd76d65d86
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-28 14:58:38 +0530

    add main method to initialize Scanner and StudentOperations in Main class

--- Main.java
+++ Main.java
@@ -5,5 +5,12 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        // creating Scanner object
+        Scanner sc = new Scanner(System.in);
+        // creating StudentOperations object
+        StudentOperations operations = new StudentOperations();
+        int choice;
 
+    }
 }
